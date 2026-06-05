8bfb625977eac2a7d1a7c3356a01d3ac4388f75e
ThoLe <tholhlearning@gmail.com>
2026-06-05 10:16:51 +0700

    finish

--- 13. LangGraph MCP/airbnb_mcp.py
+++ 13. LangGraph MCP/airbnb_mcp.py
@@ -1,12 +1,13 @@
 """Simple Airbnb MCP module."""
 
+import operator
+from langchain_core.tools import StructuredTool
 from langchain_ollama import ChatOllama
 from langgraph.graph import StateGraph, START, END
 from langgraph.prebuilt import ToolNode, tools_condition
 from langchain_core.messages import HumanMessage
 from langfuse.langchain import CallbackHandler
 from typing_extensions import TypedDict, Annotated
-import operator
 
 
 
@@ -31,7 +32,7 @@ from langchain_mcp_adapters.client import MultiServerMCPClient
 import asyncio
 
 
-async def get_tools():
+async def get_tools() -> list[StructuredTool]:
 
     client = MultiServerMCPClient(
         {
@@ -42,23 +43,19 @@ async def get_tools():
             }
         }
     )
-
-
     tools = await client.get_tools()
-
     print(f"Loaded {len(tools)} Tools")
     print(f"Tools Available: {tools}")
-
     return tools
 
 
 async def agent_node(state: AgentState):
     tools = await get_tools()
-
     llm_with_tools = llm.bind_tools(tools)
-
-    response = llm_with_tools.invoke(state['messages'])
-
+    response = llm_with_tools.invoke(
+        state['messages'],
+        config={"callbacks": [langfuce_trace]},
+    )
     return {'messages': [response]}
 
 
@@ -69,29 +66,28 @@ async def create_agent():
     builder.add_node('agent', agent_node)
     builder.add_node('tools', ToolNode(tools))
 
-
     # add edges
     builder.add_edge(START, 'agent')
     builder.add_edge('tools', 'agent')
-
     builder.add_conditional_edges('agent', tools_condition)
 
     graph = builder.compile()
-
     return graph
 
 
 async def search(query):
     agent = await create_agent()
-    result = await agent.ainvoke({'messages': [HumanMessage(query)]})
-
+    result = await agent.ainvoke(
+        {'messages': [HumanMessage(query)]},
+        config={"callbacks": [langfuce_trace]}
+    )
     response = result['messages'][-1].content
-
     print(response)
 
     return response
 
 
-if __name__=="__main__":
-    query = "Show me premium hotels for party in Mumbai"
+if __name__ == "__main__":
+    # asyncio.run(get_tools())
+    query = "Show me premium hotels for party in Ho Chi Minh City today."
     asyncio.run(search(query))
